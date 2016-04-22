c0448755702cd1ab480ff14045e2f19e98549ac3
gimmechocolate <gimmechocolate6@gmail.com>
2016-04-22 02:53:54 -0700

    Cleaned up a bit, fixed some errors I made

---
+++ SpaceGame/Assets/Scripts/JakeScripts/BlockController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class BlockController : MonoBehaviour {
+
+    
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //For Mouse Related Ventures:
+
+}
+
+
